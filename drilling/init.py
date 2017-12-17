7938e96acba5516a82b747368a7cf55cf31b4c93
zhangjianqi <zhangjianqi@bytedance.com>
2017-12-17 23:27:34 +0800

    fix encode bug
    Change-Id: I3cfa76538911a8d9ea10778a23fce2965d5c054c

--- drilling/init.py
+++ drilling/init.py
@@ -15,35 +15,35 @@ def init_base_info(site):
     get_tank_value(site)
     get_tank_temperature(site)
     get_tank_grade(site)
-    print '{0}： 成功获取油罐信息'.format(site)
+    print '{0}:成功获取油罐信息'.format(site).encode('utf-8')
 
     get_sup(site)
     get_rev(site)
-    print '{0}: 成功获取运送基本信息'.format(site)
+    print '{0}: 成功获取运送基本信息'.format(site).encode('utf-8')
 
     get_first_classify(site)
     get_second_classify(site)
     get_third_classify(site)
-    print '{0}: 成功获取分类基本信息'.format(site)
+    print '{0}: 成功获取分类基本信息'.format(site).encode('utf-8')
 
     get_inventories(site)
-    print '{0}: 成功获取库存基本信息'.format(site)
+    print '{0}: 成功获取库存基本信息'.format(site).encode('utf-8')
 
 
 def init_day_record(site, st, et):
     get_inventory_record(site, st, et)
-    print '{0}: 成功获取{1: %Y-%m-%d}~{2: %Y-%m-%d}油库记录'.format(site, st, et)
+    print '{0}: 成功获取{1: %Y-%m-%d}~{2: %Y-%m-%d}油库记录'.format(site, st, et).encode('utf-8')
     get_delivery(site, st, et)
-    print '{0}: 成功获取{1: %Y-%m-%d}~{2: %Y-%m-%d}送货记录'.format(site, st, et)
+    print '{0}: 成功获取{1: %Y-%m-%d}~{2: %Y-%m-%d}送货记录'.format(site, st, et).encode('utf-8')
     get_fuel_order(site, st, et)
-    print '{0}: 成功获取{1: %Y-%m-%d}~{2: %Y-%m-%d}油品订单'.format(site, st, et)
+    print '{0}: 成功获取{1: %Y-%m-%d}~{2: %Y-%m-%d}油品订单'.format(site, st, et).encode('utf-8')
     get_store_order(site, st, et)
-    print '{0}: 成功获取{1: %Y-%m-%d}~{2: %Y-%m-%d}非油订单'.format(site, st, et)
+    print '{0}: 成功获取{1: %Y-%m-%d}~{2: %Y-%m-%d}非油订单'.format(site, st, et).encode('utf-8')
 
 
 def init_all():
     begin = datetime.datetime.now()
-    print '开始初始化任务 {0: %Y-%m-%d %H:%M:%S}'.format(begin)
+    print '开始初始化任务 {0: %Y-%m-%d %H:%M:%S}'.format(begin).encode('utf-8')
     site_list = session.query(Site).all()
     for site in site_list:
         init_base_info(site.slug)
@@ -58,8 +58,8 @@ def init_all():
             if et > init_et:
                 break
     end = datetime.datetime.now()
-    print '结束初始化任务 {0: %Y-%m-%d %H:%M:%S}'.format(end)
-    print '共计耗时 {0} 分钟'.format((end - begin).seconds / 60.0)
+    print '结束初始化任务 {0: %Y-%m-%d %H:%M:%S}'.format(end).encode('utf-8')
+    print '共计耗时 {0} 分钟'.format((end - begin).seconds / 60.0).encode('utf-8')
 
 
 if __name__ == '__main__':
